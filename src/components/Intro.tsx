5371f03097165cc162df6d7d76242a33a62350b3
Mr-Hera <randellhera@gmail.com>
2023-07-19 18:29:16 +0300

    Feat: added state tlide + sep tractate to keep track of initial , login loader & redirect

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,19 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import 'swiper/css';
+import './Intro.css';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import Intro1Svg from '../assets/intro/1.svg'
+import Intro2Svg from '../assets/intro/2.svg'
+import Intro3Svg from '../assets/intro/3.svg'
 
-const Intro: React.FC = () => {
+interface ContainerProps {
+    onFinish: () => void;
+};
+
+const SwiperButtonNext = ({ children }: any) => {
+    const swiper = useSwiper();
+    return <IonButton onClick={() => swiper.slideNext()}>{ children }</IonButton>;
+}
+
+const Intro: React.FC<ContainerProps> = ({ onFinish }) => {
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
-            </IonContent>
-        </IonPage>
+        <Swiper>
+            <SwiperSlide>
+                <img src={Intro1Svg} alt="Intro 1" />
+                <IonText>
+                    <h3>Build amazing apps with Ionic UI components!</h3>
+                </IonText>
+                <SwiperButtonNext>Next</SwiperButtonNext>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Intro2Svg} alt="Intro 2" />
+                <IonText>
+                    <h3>Build amazing apps with Ionic UI components!</h3>
+                </IonText>
+                <SwiperButtonNext>Next</SwiperButtonNext>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Intro3Svg} alt="Intro 3" />
+                <IonText>
+                    <h3>Build amazing apps with Ionic UI components!</h3>
+                </IonText>
+                <IonButton onClick={() => onFinish()}>Finish</IonButton>
+            </SwiperSlide>
+        </Swiper>
     );
 };
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,88 @@
-import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar, useIonLoading, useIonRouter } from '@ionic/react';
 import { logInOutline, personCircleOutline } from 'ionicons/icons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginLogo from '../assets/login.svg'
+import Intro from '../components/Intro';
+import { Preferences } from '@capacitor/preferences';
+
+const INTRO_KEY = 'intro-seen';
 
 const Login: React.FC = () => {
     const router = useIonRouter();
-    const [introSeen, setIntroSeen] = useState(false);
+    const [introSeen, setIntroSeen] = useState(true);
+    const [present, dismiss] = useIonLoading();
+
+    useEffect(() => {
+        const checkStorage = async () => {
+            const seen = await Preferences.get({ key: INTRO_KEY });
+            console.log('🚀 ~ file: Login.tsx:17 ~ checkStorage ~ seen:', seen);
+            setIntroSeen(seen.value === 'true');
+        };
+
+        checkStorage();
+    }, []);
     
-    const login = (e: any) => {
+    const login = async (e: any) => {
         e.preventDefault();
-        console.log('Login');
-        // router.push('/');
-    }
+        await present('Logging in...');
+        setTimeout(async () => {
+            dismiss();
+            router.push('/app', 'root');
+        }, 2000);
+    };
+    
+    const finishIntro = async () => {
+        console.log('Finished');
+        setIntroSeen(true);
+        Preferences.set({key: INTRO_KEY, value: 'true'});
+    };
+    
+    const seeIntroAgain = async () => {
+        console.log('Finished');
+        setIntroSeen(false);
+        Preferences.remove({key: INTRO_KEY});
+    };
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar color={'success'}>
-                    <IonTitle>Ionic App</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent scrollY={false}>
-                <div className="ion-text-center ion-padding">
-                    <img src={LoginLogo} alt="Login" width={'50%'} />
-                </div>
-                <IonCard>
-                    <IonCardContent>
-                        <form onSubmit={login}>
-                            <IonInput fill='outline' labelPlacement='floating' label='email' type='email' placeholder='example@example.com'></IonInput>
-                            <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='password' type='password'></IonInput>
-                            <IonButton className='ion-margin-top' type='submit' expand='block'>
-                                Login
-                                <IonIcon icon={logInOutline} slot='end'></IonIcon>
-                            </IonButton>
-                            <IonItem className='ion-margin-top' lines="none">
-                                <IonLabel>Don't have an account?</IonLabel>
-                            </IonItem>
-                            <IonButton routerLink='/register' color={'secondary'} className='ion-margin-top' type='submit' expand='block'>
-                                Create Account
-                                <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
-                            </IonButton>
-                        </form>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
-        </IonPage>
+        <>
+            {!introSeen ? ( <Intro onFinish={finishIntro} /> ) : (
+                <IonPage>
+                    <IonHeader>
+                        <IonToolbar color={'success'}>
+                            <IonTitle>Ionic App</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+                    <IonContent scrollY={false}>
+                        <div className="ion-text-center ion-padding">
+                            <img src={LoginLogo} alt="Login" width={'50%'} />
+                        </div>
+                        <IonCard>
+                            <IonCardContent>
+                                <form onSubmit={login}>
+                                    <IonInput fill='outline' labelPlacement='floating' label='email' type='email' placeholder='example@example.com'></IonInput>
+                                    <IonInput className='ion-margin-top' fill='outline' labelPlacement='floating' label='password' type='password'></IonInput>
+                                    <IonButton className='ion-margin-top' type='submit' expand='block'>
+                                        Login
+                                        <IonIcon icon={logInOutline} slot='end'></IonIcon>
+                                    </IonButton>
+                                    <IonItem className='ion-margin-top' lines="none">
+                                        <IonLabel>Don't have an account?</IonLabel>
+                                    </IonItem>
+                                    <IonButton routerLink='/register' color={'secondary'} className='ion-margin-top' type='submit' expand='block'>
+                                        Create Account
+                                        <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
+                                    </IonButton>
+                                    <IonButton onClick={seeIntroAgain} fill='clear' size='small' color={'medium'} className='ion-margin-top' type='submit' expand='block'>
+                                        See intro again
+                                        <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
+                                    </IonButton>
+                                </form>
+                            </IonCardContent>
+                        </IonCard>
+                    </IonContent>
+                </IonPage>
+            )}
+        </>
     );
 };
 
